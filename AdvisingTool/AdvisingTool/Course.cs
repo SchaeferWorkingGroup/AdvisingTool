8cb6e6fe01e4b5d4a81c4115ad65fc65e75799b6
dfmutz <dfmutz@hotmail.com>
2015-09-25 11:43:36 -0400

    Initial Prototype of Backend

---
+++ AdvisingTool/AdvisingTool/Course.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvisingTool
+{
+    class Course
+    {
+        private int course_number;
+        private int section_number;
+        private int course_hours;
+        private string course_type;
+        private string course_name;
+        private string course_description;
+
+        private readonly string[] coursetype = { "LEC", "IND","RES", "THS", "LAB" };
+
+
+        //Course constructor
+        public Course(int c_number, int s_number, int c_hours, int c_type, string c_name, string c_description)
+        {
+            course_number = c_number;
+            section_number = s_number;
+            course_hours = c_hours;
+            course_type = coursetype[c_type];
+            course_name = c_name;
+            course_description = c_description;
+
+        }
+    }
+}
---
+++ AdvisingTool/AdvisingTool/Department.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvisingTool
+{
+    class Department
+    {
+        private string department_name;
+        LinkedList<Course> courses;
+
+        public Department()
+        {
+            courses = new LinkedList<Course>(courses);
+
+        }
+
+        public void AddCourses(int course_number, int section_number, string course_name, string course_description)
+        {
+            //adds courses into linked list from file
+        }
+
+        private void DeleteCourse(string course_name)
+        {
+            //deletes course by course name
+        }
+
+        private void DeleteCourse (int course_number)
+        {
+            //deletes course by course number
+        }
+
+
+    /*
+        public Course SearchCourse(string course_name)
+        {
+            Course searched_course;
+            //search courses by name
+            return searched_course;
+        }
+
+        public Course SearchCourse(int course_number)
+        {
+            Course searched_course;
+            //search course by number
+            return searched_course;
+        }
+        */
+
+    }
+}
